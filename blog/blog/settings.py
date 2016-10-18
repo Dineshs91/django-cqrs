afbab790274dd17240cc8073a713b0559efd4477
dinesh <dineshpy07@gmail.com>
2016-10-18 22:29:35 +0530

    Use the name postgres_db

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'postgres_db',
         'PORT': 5432
     },
     'events': {
