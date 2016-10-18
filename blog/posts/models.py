970da92c2a86d99be27f03356d7505f3bcbff202
dinesh <dineshpy07@gmail.com>
2016-10-18 23:31:15 +0530

    Add post model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
