ea34d2bd8c712a3868a757400465ebfac96ac0e6
dinesh <dineshpy07@gmail.com>
2016-10-19 13:49:11 +0530

    Add views in posts app

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+class PostView(View):
+    def get(self, request):
+        context = RequestContext(request)
+        context_dict = {}
+        return render_to_response('posts.html', context_dict, context)
+
+    def post(self, request):
+        return HttpResponse('hi')
