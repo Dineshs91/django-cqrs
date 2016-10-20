29a23e4dcbc7ac230211a9753092b2f50bdad072
dinesh <dineshpy07@gmail.com>
2016-10-20 23:27:59 +0530

    Add an entrypoint for web service
    This will take care of running the necessary commands
    only after the database is up and running

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+set -e
+
+until psql -h "postgres_db" -U "postgres" -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
 # Run python migrations
-python manage.py migrate
+python blog/manage.py migrate
+python blog/manage.py runserver 0.0.0.0:8000
