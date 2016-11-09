520cdf38cf95cf0cc85534a3e321faafc578c54d
dinesh <dineshpy07@gmail.com>
2016-11-09 09:50:43 +0530

    Handle edit form

--- blog/static/js/posts.js
+++ blog/static/js/posts.js
@@ -2,16 +2,42 @@ $(function() {
     // Initialize modal
     $('#modal1').modal();
 
-    $('form').submit(function(e) {
+    $('.create_post').submit(function(e) {
         e.preventDefault();
 
         var action = $(this).attr('action');
         var data = $(this).serialize();
-        $.post(action, data, function(data) {
+        $.post(action, data, function(res) {
             // Replace the entire content
             document.open();
-            document.write(data);
+            document.write(res);
             document.close();
         });
+    });
+
+    var post_id;
+    $('.secondary-content').click(function(e) {
+        post_id = $(this).attr('data');
+
+        $('#post_id').val(post_id);
+    });
+
+    $('.update_post').submit(function(e) {
+        e.preventDefault();
+
+        var action = $(this).attr('action') + post_id + '/';
+        var data = $(this).serialize();
+        $.ajax({url: action, type: 'PUT', data: data, success: function(res) {
+            // Iterate through the entire list and find the element
+            // Whose element's data matches with the data value.
+            $('.collection-item').each(function() {
+                var data = $(this).find('a').attr('data');
+                if (data === post_id) {
+                    // Replace the content of this list.
+                    $(this).html(res);
+                    return;
+                }
+            });
+        }});
     });
 });
