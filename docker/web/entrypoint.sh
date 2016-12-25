079fbbd4d96bf00629ff6b8971c51aa5e01ade24
DINESH S <dineshpy07@gmail.com>
2016-12-25 23:10:55 +0530

    Add a echo statement in entrypoint

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-# set -e
+set -e
 
 until psql -h "postgres_db" -U "postgres" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
-# echo "Hello"
+echo "Hello"
 
 # Run python migrations
 # python blog/manage.py migrate
