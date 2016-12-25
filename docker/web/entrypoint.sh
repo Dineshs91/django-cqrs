032f54af4f1376b8394c46b52f74d3ec9ad73f6d
DINESH S <dineshpy07@gmail.com>
2016-12-25 23:30:52 +0530

    Update entrypoint.sh

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-until psql -h "postgres_db" -U "postgres" -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+#until psql -h "postgres_db" -U "postgres" -c '\l'; do
+#  >&2 echo "Postgres is unavailable - sleeping"
+#  sleep 1
+#done
 
+
+echo "Testing1232"
 echo "Hello"
+
+ls -l
+
+echo "howdie"
 
 # Run python migrations
 # python blog/manage.py migrate
