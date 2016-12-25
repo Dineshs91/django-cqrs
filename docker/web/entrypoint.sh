b30287d3aab9342760b5fd66e8195b6edf745313
DINESH S <dineshpy07@gmail.com>
2016-12-25 10:47:32 +0530

    Run shell replay_three from here

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -7,6 +7,11 @@ until psql -h "postgres_db" -U "postgres" -c '\l'; do
   sleep 1
 done
 
+echo "Hello"
+
 # Run python migrations
 python blog/manage.py migrate
+
+sh blog/demo/replay_three.sh
+
 python blog/manage.py runserver 0.0.0.0:8000
