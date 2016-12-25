4ea62e25b39a16e199edef9e19cb842bb77ae6a7
DINESH S <dineshpy07@gmail.com>
2016-12-25 15:45:05 +0530

    Update entrypoint.sh

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -7,11 +7,11 @@ until psql -h "postgres_db" -U "postgres" -c '\l'; do
   sleep 1
 done
 
-echo "Hello"
+# echo "Hello"
 
 # Run python migrations
 python blog/manage.py migrate
 
-sh blog/demo/replay_three.sh
+# sh blog/demo/replay_three.sh
 
 # python blog/manage.py runserver 0.0.0.0:8000
